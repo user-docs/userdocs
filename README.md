f64b21e2392042cc5ef97af529b12ed8a8a93396
John Davenport <johns10davenport@gmail.com>
2021-11-15 18:34:18 -0500

    Root files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Userdocs.Umbrella
---
+++ mix.exs
@@ -0,0 +1,40 @@
+defmodule Userdocs.Umbrella.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      apps_path: "apps",
+      version: "0.1.0",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      applications: [:userdocs_web, :userdocs_desktop]
+    ]
+  end
+
+  defp deps do
+    []
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  #
+  # Aliases listed here are available only for this project
+  # and cannot be accessed from applications inside the apps/ folder.
+  defp aliases do
+    [
+      # run `mix setup` in all child apps
+      setup: ["cmd mix setup"]
+    ]
+  end
+end
