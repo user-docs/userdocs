d46878c76bdd4a5472b1c1c69282eb0eb7bfcaa9
John Davenport <johns10davenport@gmail.com>
2022-03-17 01:29:48 -0400

    Put step instances on localrepo

--- apps/userdocs/lib/userdocs/userdocs/step_instances.ex
+++ apps/userdocs/lib/userdocs/userdocs/step_instances.ex
@@ -5,17 +5,57 @@ defmodule Userdocs.StepInstances do
   require Logger
   import Ecto.Query, warn: false
   alias Schemas.StepInstances.StepInstance
+  alias Userdocs.LocalRepo
 
-  def create_step_instance(attrs \\ %{}) do
+  def list_step_instances(opts \\ %{}) do
+    filters = Map.get(opts, :filters, [])
+    base_steps_query()
+    |> maybe_filter_by_step_id(filters[:step_id])
+    |> LocalRepo.all()
+  end
+
+  def get_step_instance!(step_instance_id) do
+    from(step_instance in StepInstance, where: step_instance.id == ^step_instance_id)
+    |> LocalRepo.one!()
+  end
+
+  def create_step_instance(attrs) do
+    maybe_trim_step_instances(attrs)
     %StepInstance{}
     |> StepInstance.changeset(attrs)
-    |> Ecto.Changeset.apply_action(:insert)
+    |> LocalRepo.insert()
+  end
+
+  defp maybe_trim_step_instances(%{"step_id" => step_id}), do: maybe_trim_step_instances(step_id)
+  defp maybe_trim_step_instances(%{step_id: step_id}), do: maybe_trim_step_instances(step_id)
+  defp maybe_trim_step_instances(step_id) when is_binary(step_id) do
+    case list_step_instances(%{filters: [step_id: step_id]}) do
+      [_, _, _, _ | tail] -> delete_step_instances(tail)
+      list_of_5_or_less -> list_of_5_or_less
+    end
+  end
+
+  defp delete_step_instances(steps) do
+    Enum.each(steps, &delete_step_instance/1)
+  end
+
+  def delete_step_instance(%StepInstance{} = step_instance) do
+    LocalRepo.delete(step_instance)
+  end
+
+  defp base_steps_query(), do: from(step_instances in StepInstance, order_by: [desc: step_instances.inserted_at])
+
+  defp maybe_filter_by_step_id(query, nil), do: query
+  defp maybe_filter_by_step_id(query, step_id) do
+    from(step_instance in query,
+      where: step_instance.step_id == ^step_id,
+    )
   end
 
   def update_step_instance(%StepInstance{} = step_instance, attrs) do
     step_instance
     |> StepInstance.changeset(attrs)
-    |> Ecto.Changeset.apply_action(:update)
+    |> LocalRepo.update()
   end
 
   def count_completed_step_instances([]), do: 0
--- apps/userdocs/test/userdocs/userdocs/step_instances_test.exs
+++ apps/userdocs/test/userdocs/userdocs/step_instances_test.exs
@@ -1,6 +1,53 @@
 defmodule Userdocs.StepInstancesTest do
-	use ExUnit.Case
-	doctest Userdocs.StepInstances
+  use Userdocs.DataCase
+	alias Schemas.StepInstances.StepInstance
 	alias Userdocs.StepInstances
 
+  @valid_attrs %{"status" => "succeeded", "step_id" => Ecto.UUID.generate()}
+  @update_attrs %{"status" => "failed", "step_id" => Ecto.UUID.generate()}
+  @invalid_attrs %{"status" => "enchanting", "step_id" => Ecto.UUID.generate()}
+
+  def step_instance_fixture(attrs \\ %{}) do
+    {:ok, step_instance} =
+      attrs
+      |> Enum.into(@valid_attrs)
+      |> StepInstances.create_step_instance()
+
+    step_instance
+  end
+
+  describe "Step Instances" do
+    test "list_step_instances/0 returns all step_instances" do
+      Userdocs.LocalRepo.delete_all(StepInstance)
+      step_instance = step_instance_fixture()
+      assert StepInstances.list_step_instances() == [step_instance]
+    end
+
+    test "create_step_instance/1 with valid data creates a step_instances" do
+      assert {:ok, %StepInstance{} = step_instance} = StepInstances.create_step_instance(@valid_attrs)
+      assert step_instance.status == :succeeded
+    end
+
+    test "create_step_instance/1 allows 5 records per step id" do
+      Enum.each(1..7, fn _ -> StepInstances.create_step_instance(@valid_attrs) end)
+      step_instances = StepInstances.list_step_instances(%{filters: [step_id: @valid_attrs["step_id"]]})
+      assert Enum.count(step_instances) == 5
+    end
+
+    test "create_step_instance/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = StepInstances.create_step_instance(@invalid_attrs)
+    end
+
+    test "update_step_instance/2 with valid data updates the step_instance" do
+      step_instance = step_instance_fixture()
+      assert {:ok, %StepInstance{} = step_instance} = StepInstances.update_step_instance(step_instance, @update_attrs)
+      assert step_instance.status == :failed
+    end
+
+    test "update_step_instance/2 with invalid data returns error changeset" do
+      step_instance = step_instance_fixture()
+      assert {:error, %Ecto.Changeset{}} = StepInstances.update_step_instance(step_instance, @invalid_attrs)
+      assert step_instance == StepInstances.get_step_instance!(step_instance.id)
+    end
+  end
 end
