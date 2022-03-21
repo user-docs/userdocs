7f083eb81e3fa54d6244132ac1b32dfaf1cd428d
John Davenport <johns10davenport@gmail.com>
2022-03-20 22:03:06 -0400

    Because they were reused

--- apps/userdocs/test/support/annotation_fixtures.ex
+++ apps/userdocs/test/support/annotation_fixtures.ex
@@ -1,6 +1,9 @@
 defmodule Userdocs.AnnotationFixtures do
   @moduledoc false
   alias Userdocs.Annotations
+  alias Schemas.Annotations.Annotation
+  alias Schemas.Elements.ElementAnnotation
+  alias Schemas.Elements.Element
 
   def annotation(attrs, opts) do
     {:ok, annotation} = annotation_attrs(attrs) |> Annotations.create_annotation(opts)
@@ -10,20 +13,6 @@ defmodule Userdocs.AnnotationFixtures do
   def annotation_struct(attrs) do
     {:ok, annotation } = annotation_attrs(attrs) |> Annotations.create_annotation_struct()
     annotation
-  end
-
-  def badge_annotation_attrs(attrs) do
-    %{
-      page_id: Ecto.UUID.generate(),
-      label: "1",
-      color: "green",
-      font_size: 16,
-      x_offset:  0,
-      x_orientation: "R",
-      y_offset: 0,
-      y_orientation: "M"
-    }
-    |> Map.merge(attrs)
   end
 
   def annotation_attrs(attrs) do
@@ -40,6 +29,58 @@ defmodule Userdocs.AnnotationFixtures do
       x_orientation: "some x_orientation",
       y_offset: 42,
       y_orientation: "some y_orientation"
+    }
+    |> Map.merge(attrs)
+  end
+
+  def badge_annotation_struct(attrs \\ %{}) do
+    %Annotation{
+      id: UUID.uuid4(),
+      annotation_type_id: "badge",
+      label: "1",
+      element_annotations: [
+        %ElementAnnotation{element: %Element{strategy_id: "css", selector: "h1"}}
+      ]
+    }
+    |> Map.merge(attrs)
+  end
+
+  def outline_annotation_struct(attrs \\ %{}) do
+    %Annotation{
+      id: UUID.uuid4(),
+      annotation_type_id: "outline",
+      thickness: "4",
+      color: "green",
+      element_annotations: [
+        %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//html"}},
+        %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//body"}}
+      ]
+    }
+    |> Map.merge(attrs)
+  end
+
+  def badge_outline_struct(attrs \\ %{}) do
+    %Annotation{
+      id: UUID.uuid4(),
+      annotation_type_id: "badge_outline",
+      thickness: "4",
+      label: "4",
+      color: "green",
+      element_annotations: [
+        %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//html"}},
+        %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//body"}}
+      ]
+    }
+    |> Map.merge(attrs)
+  end
+
+  def blur_annotation_struct(attrs \\ %{}) do
+    %Annotation{
+      id: UUID.uuid4(),
+      annotation_type_id: "blur",
+      element_annotations: [
+        %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//html"}}
+      ]
     }
     |> Map.merge(attrs)
   end
