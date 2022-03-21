1a2ea9211bb0a4c233f4b27abe631cead293acff
John Davenport <johns10davenport@gmail.com>
2022-03-20 22:08:24 -0400

    Refactor fixtures, add integration tests

--- apps/browser_controller/test/browser_controller/annotation_handler_test.exs
+++ apps/browser_controller/test/browser_controller/annotation_handler_test.exs
@@ -1,73 +1,61 @@
 defmodule BrowserController.AnnotationsTest do
   use ExUnit.Case
   alias BrowserController.AnnotationHandler
-  alias Schemas.Annotations.Annotation
-  alias Schemas.Elements.ElementAnnotation
-  alias Schemas.Elements.Element
+  alias BrowserController.Browser
+  alias BrowserController.Utilities
+  alias Userdocs.AnnotationFixtures
 
   describe "Annotations Handler" do
     test "builds badge annotation properly" do
-      result =
-        %Annotation{
-          id: UUID.uuid4(),
-          annotation_type_id: "badge",
-          label: "1",
-          element_annotations: [
-            %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//html"}}
-          ]
-        }
-        |> AnnotationHandler.cast()
-
-      assert result =~ "window.element = window.getElement('css', '//html')"
+      result = AnnotationHandler.cast(AnnotationFixtures.badge_annotation_struct())
+      assert result =~ "window.element = window.getElement"
     end
 
     test "builds outline annotation properly" do
-      result =
-        %Annotation{
-          id: UUID.uuid4(),
-          annotation_type_id: "outline",
-          thickness: "4",
-          color: "green",
-          element_annotations: [
-            %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//html"}},
-            %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//body"}}
-          ]
-        }
-        |> AnnotationHandler.cast()
-
+      result = AnnotationHandler.cast(AnnotationFixtures.outline_annotation_struct())
       assert result =~ "window.elements.push({strategy: 'css', selector: '//body'})"
     end
 
     test "builds badge outline annotation properly" do
-      result =
-        %Annotation{
-          id: UUID.uuid4(),
-          annotation_type_id: "badge_outline",
-          thickness: "4",
-          label: "4",
-          color: "green",
-          element_annotations: [
-            %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//html"}},
-            %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//body"}}
-          ]
-        }
-        |> AnnotationHandler.cast()
-
+      result = AnnotationHandler.cast(AnnotationFixtures.badge_outline_struct())
       assert result =~ "window.elements.push({strategy: 'css', selector: '//html'})"
     end
 
     test "builds blur annotation properly" do
-      result =
-        %Annotation{
-          id: UUID.uuid4(),
-          annotation_type_id: "blur",
-          element_annotations: [
-            %ElementAnnotation{element: %Element{strategy_id: "css", selector: "//html"}}
-          ]
-        }
-        |> AnnotationHandler.cast()
+      result = AnnotationHandler.cast(AnnotationFixtures.blur_annotation_struct())
+      assert result =~ "window.addBlurClass"
+    end
+  end
 
-      assert result =~ "window.addBlurClass(window.getElement('css', '//html'))"
+  describe "Annotations Scripts Run in Browser" do
+    setup do
+      %{headed_browser_pid: pid} = result = BrowserController.ensure_browser_open()
+      BrowserController.execute({:navigate, %{url: @test_page_path}})
+
+      annotations_script =
+        Path.join(File.cwd!(), "../userdocs_desktop_web/priv/static/assets/annotations.js")
+        |> File.read!()
+
+      Browser.execute(pid, {:evaluate_script, %{script: annotations_script}})
+      result
+    end
+
+    test "badge creates the elements", %{headed_browser_pid: browser_pid, page_pid: page_pid} do
+      annotation = AnnotationFixtures.badge_annotation_struct()
+      script = AnnotationHandler.cast(annotation)
+      Browser.execute(browser_pid, {:evaluate_script, %{script: script}})
+      locator_id = "#userdocs-annotation-#{annotation.id}-locator"
+      badge_id = "#userdocs-annotation-#{annotation.id}-badge"
+
+      {:ok, locator_node_id} = Utilities.get_node_id("css", locator_id, page_pid)
+      {:ok, locator_attributes} = Utilities.get_attributes(page_pid, locator_node_id)
+      {:ok, badge_node_id} = Utilities.get_node_id("css", badge_id, page_pid)
+      {:ok, badge_attributes} = Utilities.get_attributes(page_pid, badge_node_id)
+
+      assert locator_attributes.id == locator_id
+      assert "userdocs-locator" in locator_attributes.class
+      assert badge_attributes.id == badge_id
+      assert "userdocs-badge" in badge_attributes
     end
   end
 end
