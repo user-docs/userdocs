66a372c90dcf3f00a7b3d63c80c446fe9f4a3809
John Davenport <johns10davenport@gmail.com>
2022-03-20 20:11:50 -0400

    Needed to pass client integration tests

--- apps/browser_controller/mix.exs
+++ apps/browser_controller/mix.exs
@@ -10,6 +10,7 @@ defmodule BrowserController.MixProject do
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
       elixir: "~> 1.12",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -27,6 +28,9 @@ defmodule BrowserController.MixProject do
       }
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
---
+++ apps/browser_controller/test/support/data_case.ex
@@ -0,0 +1,22 @@
+defmodule BrowserController.DataCase do
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      import BrowserController.DataCase
+    end
+  end
+
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Userdocs.Repo)
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Userdocs.LocalRepo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(Userdocs.Repo, {:shared, self()})
+      Ecto.Adapters.SQL.Sandbox.mode(Userdocs.LocalRepo, {:shared, self()})
+    end
+
+    :ok
+  end
+
+end
