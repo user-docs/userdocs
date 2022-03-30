64f0481e2ce975a4935ab097d3a1534a79567d9e
John Davenport <johns10davenport@gmail.com>
2022-03-30 00:00:41 -0400

    terst

--- apps/browser_controller/test/browser_controller_test.exs
+++ apps/browser_controller/test/browser_controller_test.exs
@@ -1,6 +1,12 @@
 defmodule BrowserController.BrowserControllerTest do
   use ExUnit.Case
 
+  alias BrowserController.Queue
+
+  defp initial_state(run_state) do
+    %{queue: {[], []}, run_state: run_state}
+  end
+
   describe "Execution" do
     alias Schemas.StepInstances.StepInstance
     alias Schemas.Steps.Step
@@ -19,10 +25,31 @@ defmodule BrowserController.BrowserControllerTest do
       Map.put(context, :user, user)
     end
 
-    test "works", %{nothing: %{id: id}} do
-      result = BrowserController.handle_execute(%{queue: {[], []}}, {:execute_step, %{step_id: id}}) |> IO.inspect()
+    test "executing a normal command completes the queue, and returns to pause" do
+      command = {:do_nothing, %{}}
+      BrowserController.execute(command)
+      assert_receive(%{event: "queue_updated", payload: %{queue: [command]}})
+      assert_receive(%{event: "run_state_updated", payload: %{run_state: :play}})
+      assert_receive(%{event: "queue_updated", payload: %{queue: []}})
+      assert_receive(%{event: "run_state_updated", payload: %{run_state: :pause}})
+      assert BrowserController.run_state() == :pause
+    end
 
-      assert %{queue: {[], []}} = result
+    test "executing a failing command clears the queue, and returns to pause" do
+      works = {:do_nothing, %{}}
+      fails = {:error, %{}}
+      BrowserController.execute([works, fails, works])
+      assert_receive(%{event: "queue_updated", payload: %{queue: [works, fails, works]}})
+      assert_receive(%{event: "run_state_updated", payload: %{run_state: :play}})
+      assert_receive(%{event: "queue_updated", payload: %{queue: [fails, works]}})
+      assert_receive(%{event: "execution_error", payload: %{message: "This command did nothing."}})
+      assert_receive(%{event: "queue_updated", payload: %{queue: []}})
+      assert_receive(%{event: "run_state_updated", payload: %{run_state: :pause}})
+      assert BrowserController.run_state() == :pause
+    end
+
+    test "executes a step instance", %{nothing: %{id: id}} do
+      BrowserController.execute({:execute_step, %{step_id: id}})
       assert_received(%{event: "create", payload: %StepInstance{}})
       assert_received(%{event: "queue_updated"})
       assert_received(%{event: "update", payload: %StepInstance{}})
