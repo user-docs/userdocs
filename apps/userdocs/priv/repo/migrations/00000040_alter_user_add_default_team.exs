3d7f66d637d608af5e4d148d901b120e2e58db05
John Davenport <johns10davenport@gmail.com>
2022-03-31 00:26:05 -0400

    We might use this user on client

--- apps/userdocs/priv/repo/migrations/00000040_alter_user_add_default_team.exs
+++ apps/userdocs/priv/repo/migrations/00000040_alter_user_add_default_team.exs
@@ -3,10 +3,8 @@ defmodule Userdocs.Repo.Migrations.AddDefaultTeam do
 
   def change do
     alter table(:users) do
-      add :selected_team_id, references(:teams, on_delete: :delete_all, type: :uuid)
-      add :selected_project_id, references(:projects, on_delete: :delete_all, type: :uuid)
+      add :selected_team_id, :uuid
+      add :selected_project_id, :uuid
     end
-    create index(:users, [:selected_team_id])
-    create index(:users, [:selected_project_id])
   end
 end
