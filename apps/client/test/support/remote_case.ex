f4a76a94e97e549b91a6c8131ebbf6b478cf40e0
John Davenport <johns10davenport@gmail.com>
2022-04-03 12:05:32 -0400

    Setups in cases!

---
+++ apps/client/test/support/remote_case.ex
@@ -0,0 +1,93 @@
+defmodule Client.RemoteCase do
+  @moduledoc """
+  This module defines the test case to be used by
+  tests that require setting up a connection.
+
+  Such tests rely on `Phoenix.ConnTest` and also
+  import other functionality to make it easier
+  to build common data structures and query the data layer.
+
+  Finally, if the test case interacts with the database,
+  we enable the SQL sandbox, so changes done to the database
+  are reverted at the end of every test. If you are using
+  PostgreSQL, you can even run database tests asynchronously
+  by setting `use UserdocsDesktopWeb.ConnCase, async: true`, although
+  this option is not recommended for other databases.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      import Client.RemoteCase
+
+      alias Userdocs.Tokens
+      alias Userdocs.UsersFixtures
+      alias Userdocs.ContextsFixtures
+      alias Userdocs.ProjectsFixtures
+      alias Userdocs.WebFixtures
+      alias Userdocs.TeamsFixtures
+
+      @remote_opts %{context: %{repo: Userdocs.Repo}}
+
+      defp ensure_web_started(_) do
+        Application.ensure_all_started(:userdocs_web)
+        :ok
+      end
+
+      defp create_password(_), do: %{password: UUID.uuid4()}
+      defp create_user(%{password: password}), do: %{user: UsersFixtures.confirmed_user(password)}
+      defp create_team(_), do: %{team: TeamsFixtures.team(@remote_opts)}
+
+      defp create_team_user(%{user: user, team: team}),
+        do: %{team_user: TeamsFixtures.team_user(user.id, team.id, @remote_opts)}
+
+      defp create_remote_strategy(_), do: %{strategy: WebFixtures.strategy(@remote_opts)}
+
+      defp create_remote_project(%{team: team, strategy: strategy}),
+        do: %{project: ProjectsFixtures.project(team.id, strategy.id, @remote_opts)}
+
+      defp create_remote_tokens(%{user: user}) do
+        pow_config = [
+          mod: UserdocsWeb.API.Auth.Plug,
+          plug: UserdocsWeb.API.Auth.Plug,
+          otp_app: :userdocs_web
+        ]
+
+        backend = Pow.Config.get(pow_config, :cache_store_backend, Pow.Store.Backend.EtsCache)
+        store_config = [backend: backend, pow_config: pow_config]
+        secret_key_base = Application.get_env(:userdocs_web, UserdocsWeb.Endpoint)[:secret_key_base]
+        conn = %Plug.Conn{secret_key_base: secret_key_base}
+        access_token = Pow.UUID.generate()
+        renewal_token = Pow.UUID.generate()
+        signed_access_token = sign_token(conn, access_token, pow_config)
+        signed_renewal_token = sign_token(conn, Pow.UUID.generate(), pow_config)
+
+        Pow.Store.CredentialsCache.put(
+          store_config,
+          access_token,
+          {user, [renewal_token: signed_renewal_token]}
+        )
+
+        PowPersistentSession.Store.PersistentSessionCache.put(
+          store_config,
+          renewal_token,
+          {user, [access_token: signed_access_token]}
+        )
+
+        %{access_token: signed_access_token, renewal_token: signed_renewal_token}
+      end
+
+      defp sign_token(conn, token, config),
+        do: Pow.Plug.sign_token(conn, Atom.to_string(UserdocsWeb.API.Auth.Plug), token, config)
+    end
+  end
+
+  setup tags do
+    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Userdocs.Repo, shared: not tags[:async])
+    on_exit(fn ->
+      Ecto.Adapters.SQL.Sandbox.stop_owner(pid)
+    end)
+    {:ok, %{pid: pid}}
+  end
+end
