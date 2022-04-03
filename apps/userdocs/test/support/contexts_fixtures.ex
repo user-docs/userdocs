5f744b42a74367c2f2ea5d99fec1bcf623c00230
John Davenport <johns10davenport@gmail.com>
2022-04-03 00:12:00 -0400

    Make tests betta

---
+++ apps/userdocs/test/support/contexts_fixtures.ex
@@ -0,0 +1,19 @@
+defmodule Userdocs.ContextsFixtures do
+  alias Userdocs.Contexts
+
+  def context(attrs, opts) do
+    {:ok, context} =
+      context_attrs(attrs)
+      |> Contexts.create_context(opts)
+    context
+  end
+
+  def context_attrs(attrs) do
+    %{
+      team_id: UUID.uuid4(),
+      user_id: UUID.uuid4(),
+      project_id: UUID.uuid4()
+    }
+    |> Map.merge(attrs)
+  end
+end
