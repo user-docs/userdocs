68ba747b2b090ef2de473b84f4b71c216f23917e
John Davenport <johns10davenport@gmail.com>
2022-04-05 02:00:43 -0400

    use impl and @behavior

--- apps/client/lib/client/projects.ex
+++ apps/client/lib/client/projects.ex
@@ -1,44 +1,23 @@
 defmodule Client.Projects do
-  import Client.Helpers
+  import Client.APISupport
   import Client.Constants
-  alias Client.Remote
   alias Schemas.Projects.Project
 
+  @callback list_projects(map()) :: list(Project)
+  @callback create_project(map(), map()) :: {:ok, Project} | {:error, Ecto.Changeset}
+  @callback update_project(Project, map(), map()) :: {:ok, Project} | {:error, Ecto.Changeset}
+  @callback delete_project(binary(), map()) :: {:ok, Project} | {:error, Ecto.Changeset}
+
+  def create_project(attrs, state),
+    do: impl(state).create_project(attrs, local_or_remote_opts(state))
+
+  def update_project(project, attrs, state),
+    do: impl(state).update_project(project, attrs, local_or_remote_opts(state))
+
+  def delete_project(id, state), do: impl(state).delete_project(id, local_or_remote_opts(state))
+
   def load_projects(state, opts) do
-    case is_remote?(state) do
-      true -> load_remote_projects(state, merge_token(opts, state))
-      false -> load_local_projects(state, merge_local_opts(opts))
-    end
-  end
-
-  defp load_remote_projects(state, opts) do
-    projects = Client.Remote.Projects.list_projects(opts)
+    projects = impl(state).list_projects(local_or_remote_opts(state, opts))
     StateHandlers.load(state, projects, Project, state_opts())
-  end
-
-  defp load_local_projects(state, opts) do
-    projects = Userdocs.Projects.list_projects(opts)
-    StateHandlers.load(state, projects, Project, state_opts())
-  end
-
-  def create_project(attrs, state) do
-    case is_remote?(state) do
-      true -> Remote.Projects.create_project(attrs, state)
-      false -> Userdocs.Projects.create_project(attrs, local_opts())
-    end
-  end
-
-  def update_project(project, attrs, state) do
-    case is_remote?(state) do
-      true -> Remote.Projects.update_project(project, attrs, state)
-      false -> Userdocs.Projects.update_project(project, attrs, local_opts())
-    end
-  end
-
-  def delete_project(id, state) do
-    case is_remote?(state) do
-      true -> Remote.Projects.delete_project(id, state)
-      false -> Userdocs.Projects.delete_project(id, local_opts())
-    end
   end
 end
